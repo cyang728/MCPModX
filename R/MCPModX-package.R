884380ae7e0c12772cfd2cec7fbd6280c656fa4e
cyang728 <cheng.han.yang@uth.tmc.edu>
2024-08-06 04:38:29 -0400

    import mvtnorm.control and qmvt

--- R/MCPModX-package.R
+++ R/MCPModX-package.R
@@ -4,6 +4,8 @@
 ## usethis namespace: start
 #' @importFrom DoseFinding fitMod
 #' @importFrom DoseFinding Mods
+#' @importFrom DoseFinding mvtnorm.control
 #' @importFrom DoseFinding optContr
+#' @importFrom mvtnorm qmvt
 ## usethis namespace: end
 NULL
